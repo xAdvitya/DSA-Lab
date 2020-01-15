1e02f46a8c755015911a46a372e458de144a5bf4
Advitya-sharma <advityasharma21@gmail.com>
2020-01-15 19:38:39 +0530

    fixed inserting element

--- deleting.cpp
+++ deleting.cpp
@@ -17,17 +17,4 @@ int main()
     int p, num;
     std::cout << "Enter position where you want to insert an element\n";
     std::cin >> p;
-    std::cout << "Enter number\n";
-    std::cin >> num;
-
-    for (int i = n; i > p - 1; i--)
-    {
-        arr[i] = arr[i - 1];
-    }
-    arr[p - 1] = num;
-    std::cout << "New Array elements\n";
-    for (int i = 0; i < n + 1; i++)
-    {
-        std::cout << arr[i] << std::endl;
-    }
 }
