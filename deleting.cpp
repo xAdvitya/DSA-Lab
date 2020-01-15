ba6cc09380167b25267d4e736a1fe74879e04d8b
Advitya-sharma <advityasharma21@gmail.com>
2020-01-15 19:43:25 +0530

    deleting array elements

--- deleting.cpp
+++ deleting.cpp
@@ -15,6 +15,17 @@ int main()
     }
 
     int p, num;
-    std::cout << "Enter position where you want to insert an element\n";
+    std::cout << "Enter position where you want to delete an element\n";
     std::cin >> p;
+
+    for (int i = p - 1; i < n; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    std::cout << "New Array elements\n";
+    for (int i = 0; i < n - 1; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
 }
