1bd2febdb3047d369f6d1bb8dfacca8b340fc04c
Advitya-sharma <advityasharma21@gmail.com>
2020-01-17 03:56:30 +0530

    2d array

---
+++ 2D array/taking_input.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    int r, c;
+
+    std::cout << "Enter number of rows and columns" << std::endl;
+    std::cin >> r >> c;
+    int arr[r][c];
+
+    std::cout << "Enter elements" << std::endl;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    std::cout << "{ " << std::endl;
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            std::cout << arr[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << " }" << std::endl;
+}
