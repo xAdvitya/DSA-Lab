beeaf73f58f294d490b341fdf6e964f134671d11
Advitya sharma <advityasharma21@gmail.com>
2020-01-24 04:11:34 +0530

    addind sorting

--- Linked list/search/linear_search.cpp
+++ Linked list/search/linear_search.cpp
@@ -29,5 +29,18 @@ int main() {
         cout<<"element not found"<<endl;
     }
 
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n-1;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+cout<<"Array elements are "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 
 }
