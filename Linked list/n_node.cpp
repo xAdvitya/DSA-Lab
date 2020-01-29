a735bc544d7307bb68738889ba2d8718f3c03c1b
Advitya sharma <advityasharma21@gmail.com>
2020-01-29 19:37:31 +0530

    linked list implemented

---
+++ Linked list/n_node.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *link;
+};
+
+int main() {
+   
+   Node *temp=NULL,*head=NULL;
+   int i=1;
+   while(i){
+       temp = new Node();
+       cout<<"Enter Data Element"<<endl;
+       cin>>temp->data;
+       if(head ==NULL){
+           head = temp;
+       }
+       else{
+           temp->link = head;
+           head = temp;
+       }
+        cout<<"Enter 1 if you want make new node \nelse zero\n";
+        cin>>i;
+   }
+   head = temp;
+   cout<<"Elements in the linked list are"<<endl;
+   while(temp!=NULL){
+       cout<<temp->data<<endl;
+       temp = temp->link;
+   }
+   
+   
+}
