0dc07d9a92d514e620d743741188c53758c11754
Advitya sharma <advityasharma21@gmail.com>
2020-02-09 20:07:44 +0530

    buggy stack

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int stx[100],top=-1;
+
+void show();
+void push(int top,int n,int value);
+void pop();
+
 int main()
 {
-    int stack[100];
     int n;
     cout<<"enter the size of stack";
     cin>>n;
@@ -12,24 +18,55 @@ int main()
         cin>>choice;
         switch(choice){
             case 1:{
-                cout<<"Enter value to enter in the stack";
+                cout<<"Enter value to enter in the stack"<<endl;
                 cin>>val;
-                push(val);
+                push(top,n,val);
+                break;
             }
             case 2:{
                 pop();
+                break;
             }
             case 3:{
+                cout<<"All value in the stack are"<<endl;
                 show();
+                break;
             }
             case 4:{
                 cout<<"program ended"<<endl;
+                break;
             }
             default:{
                 cout<<"Enter valid choice bro !"<<endl;
+                break;
             }
         }
     }
+}
 
+void push(int top,int n,int value){
+    cout<<top <<"top is "<<endl;
+    if(top<=n-1){
+        top++;
+        stx[top] = value;
+        cout<<"value pushed"<<endl;
+    }
+    else{
+        cout<<"stack overflowing "<<endl;
+    }
+}
+void pop(){
+    if(top>0){
+        cout<<"value "<<stx[top]<<" is popped"<<endl;
+        top--;
+    }
+    else{
+        cout<<"stack underflowing "<<endl;
+    }
+}
 
+void show(){
+    for(int i=0;i<=top;i++){
+        cout<<stx[i]<<endl;
+    }
 }
