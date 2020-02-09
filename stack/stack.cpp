0712a6a3ea0ec38b5f05125e322df9329f8c3508
Advitya sharma <advityasharma21@gmail.com>
2020-02-09 19:38:55 +0530

    structure

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -6,7 +6,30 @@ int main()
     int n;
     cout<<"enter the size of stack";
     cin>>n;
+    int choice = 0,val;
+    while(choice != 4){
+        cout<<"Enter your choice "<<endl;
+        cin>>choice;
+        switch(choice){
+            case 1:{
+                cout<<"Enter value to enter in the stack";
+                cin>>val;
+                push(val);
+            }
+            case 2:{
+                pop();
+            }
+            case 3:{
+                show();
+            }
+            case 4:{
+                cout<<"program ended"<<endl;
+            }
+            default:{
+                cout<<"Enter valid choice bro !"<<endl;
+            }
+        }
+    }
 
-    
 
 }
