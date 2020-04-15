3d7c3b97bfd03fe057c3f9363c3393e4f533d933
Advitya <advityasharma21@gmail.com>
2020-04-15 23:13:31 +0530

    queue

---
+++ queue/queue_ll.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Queue
+{
+public:
+int data;
+Queue *next;
+
+Queue(){
+    data =0;
+    next = nullptr;
+    }
+    void enqueue(int val);
+    void display();
+    void deque();
+};
+
+Queue *head,*tail;
+
+void Queue::enqueue(int val){
+    Queue *temp = new Queue();
+    temp->data = val;
+    if(head == nullptr){
+      tail = head = temp;
+    }
+    else
+    {  
+     tail->next = temp;
+     tail = temp;
+    }  
+}
+
+void Queue::display(){
+    
+    Queue *x = head;
+   
+    while(x != nullptr){
+        cout<<x->data<<" ";
+        x = x->next;
+    }
+}
+
+void Queue::deque(){
+    Queue *temp = head;
+    head = head->next;
+
+    delete temp;
+}
+
+int main(int argc, const char** argv) {
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    
+    q.deque();
+    q.deque();
+
+    q.display();
+
+    return 0;
+}
