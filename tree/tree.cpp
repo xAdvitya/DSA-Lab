db25f51ec95eb9420e205c437415824b4c14ae60
Advitya <advityasharma21@gmail.com>
2020-05-01 11:11:13 +0530

    traversals

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -9,7 +9,7 @@ struct node{
 node* create(){
     node *newnode = new node;
     int data;
-    cout<<"enter data // -1 for no node"<<endl;
+    cout<<"enter data or -1 for no node"<<endl;
     cin>>data;
     
     if(data == -1){
@@ -27,9 +27,41 @@ node* create(){
     }
 }
 
+void preorder(node *root){
+    if(root){
+    cout<<root->data<<" ";
+    preorder(root->leftchild);
+    preorder(root->rightchild);
+    }
+}
+
+void inorder(node *root){
+    if(root){
+    inorder(root->leftchild);
+    cout<<root->data<<" ";
+    inorder(root->rightchild);
+    }
+}
+
+void postorder(node *root){
+    if(root){
+    postorder(root->leftchild);
+    postorder(root->rightchild);
+    cout<<root->data<<" ";
+    }
+}
 
 int main(){
 
+cout<<"creating tree\n";
 node *root = create();
 
+cout<<"\npreorder traversal "<<endl;
+preorder(root);
+
+cout<<"\ninorder traversal "<<endl;
+inorder(root);
+
+cout<<"\npostorder traversal "<<endl;
+postorder(root);
 }
