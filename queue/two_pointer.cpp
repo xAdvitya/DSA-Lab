f54ab617c307640a84de1f0c05605cef7c2e5cec
Advitya <54709416+xAdvitya@users.noreply.github.com>
2023-03-06 02:39:19 +0530

    fixing bugs

--- queue/two_pointer.cpp
+++ queue/two_pointer.cpp
@@ -20,26 +20,33 @@ public:
 
 void queue::enqueue(int val){
     if(rear==size-1){
-        cout<<"Queue is full"<<endl;
-    }
-    else{
+        cout<<"full\n";
+    }else{
+        
+        if(front==-1){
+            front+=1;
+        }
         rear+=1;
-        Q[rear] = val;
+        Q[rear]=val;
     }
+    
 }
 
 void queue::deque(){
-    if(rear == front){
-        cout<<"Queue is empty\n";
-    }
-    else{
+    if(rear<front || rear==-1){
+        cout<<"empty\n";
+    }else{
         front++;
-    }
+    } 
 }
 
 void queue::display(){
-    for(int i=front+1;i<=rear;i++){
-        cout<<Q[i]<<" ";
+    if(rear<front || rear==-1){
+        cout<<"empty\n";
+    }else{
+        for(int i=front;i<=rear;i++){
+            cout<<Q[i]<<" ";
+        }
     }
 }
 
